3f1ab614e1b479dd16c74000cc452c5fcee5e37a
dainxor <daniel.leond@udea.edu.co>
2026-04-22 16:56:41 -0500

    Created a new home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardHome = () => (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h2>Dashboard</h2>
+        <p>Welcome! Use the menu to access different sections.</p>
+    </div>
+);
+
+export default DashboardHome;
+
+
